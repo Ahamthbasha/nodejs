7a886218560d801d97d8cc98dc15c41951e2c19c
ahamathbasha <bashachristopherallah786@gmail.com>
2024-08-12 19:20:33 +0530

    import export methods

---
+++ 6.importexport/math.js
@@ -0,0 +1,36 @@
+/*
+this is second pattern to run program
+module.exports=((a,b)=>{
+    return a+b;
+})*/
+
+/*
+this is third way
+const add=((a,b)=>{
+    return a+b;
+})
+const sub=((a,b)=>{
+    return a-b;
+})
+module.exports={add,sub};
+*/
+
+/*
+this is fourth way
+const add=((a,b)=>{
+    return a+b;
+})
+const sub=((a,b)=>{
+    return a-b;
+})
+module.exports={add,sub}*/
+
+/*
+this is fifth way
+exports.add=((a,b)=>{
+    return a+b; 
+})
+
+exports.sub=((a,b)=>{
+    return a-b;
+})*/
---
+++ 6.importexport/require.js
@@ -0,0 +1,26 @@
+/*
+this is second pattern to run program
+const add=require("./math")
+console.log(add(2,3))*/
+
+/*
+this is third way
+const math=require("./math")
+console.log(math.add(1,2))
+console.log(math.sub(2,1))*/
+
+/*
+this is fourth way
+const math=require("./math")
+//destructuring function in require
+const {add,sub}=math
+console.log(add(1,3))
+console.log(sub(3,1))*/
+
+/*
+this is fifth way
+const math=require("./math")
+const {add,sub}=math
+console.log(add(2,1))
+console.log(sub(2,1)) 
+*/
